8e0d2cba49169da94e230eb11033162ed7416a02
Sonryu <141044164+YasueTakeru@users.noreply.github.com>
2024-02-04 18:37:07 +0900

    トップページを実装した

---
+++ src/app.js
@@ -0,0 +1,43 @@
+// src/app.js
+const path = require("path");
+
+const PromiseRouter = require("express-promise-router");
+const express = require("express");
+
+const methodOverride = require("method-override");
+
+const PORT = 3000;
+
+const run = () => {
+  const app = express();
+
+  // テンプレートエンジンとしてejsを使うことを設定する
+  app.set("view engine", "ejs");
+  app.set("views", path.resolve(__dirname, "views"));
+
+  app.use(methodOverride("_method"));
+
+  // リクエストボディをパースする
+  app.use(express.urlencoded({ extended: true }));
+
+  // 静的ファイルを提供する
+  app.use(express.static(path.resolve(__dirname, "public")));
+
+  // ルーティング
+  const router = PromiseRouter();
+  app.use(router);
+  
+  router.get("/", (req, res) => {
+    res.render("main");
+  });
+
+  // 3000番にポート解放
+  app.listen(PORT, () => {
+    console.log(`Application running on port ${PORT}`);
+  });
+};
+
+// runをexportする。
+module.exports = {
+  run,
+};
